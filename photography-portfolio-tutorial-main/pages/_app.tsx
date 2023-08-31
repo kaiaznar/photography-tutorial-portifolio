1e209a2ed4c0230a8e19cc35c5a7051cff42b838
kaiaznar <kaiaznar2002@gmail.com>
2023-08-31 09:42:28 +0100

    first commit

---
+++ photography-portfolio-tutorial-main/pages/_app.tsx
@@ -0,0 +1,16 @@
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { Expletus_Sans } from "@next/font/google";
+
+const expletusSans = Expletus_Sans({
+  subsets: ["latin"],
+  variable: "--font-expletus",
+});
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <div className={`${expletusSans.variable} font-sans h-full`}>
+      <Component {...pageProps} />
+    </div>
+  );
+}
